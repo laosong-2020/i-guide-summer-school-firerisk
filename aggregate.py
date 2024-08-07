435f2a25d7065837b69cfe1235f0379c6da59032
laosong-2020 <songzl0606@gmail.com>
2024-08-06 23:40:55 -0600

    refactor aggregation and config files

--- aggregate.py
+++ aggregate.py
@@ -13,7 +13,7 @@ import rasterio
 import numpy as np
 from rasterio.mask import mask
 
-from config import feature_dict_list, BASE_DIR, DATASET_DIR
+from config import nri_columns, feature_dict_list, DATASET_DIR
 
 def add_column_by_feature(gdf, feature_dict):
     raster = rasterio.open(feature_dict["reprojected_file_name"])
@@ -38,13 +38,18 @@ def add_column_by_feature(gdf, feature_dict):
 
 if __name__ == "__main__":
     # Load the census tract shapefile
-    census_tracts = gpd.read_file(f"{DATASET_DIR}/tl_2023_48_tract/tl_2023_48_tract.shp")
-    target_crs = census_tracts.crs
+    census_tracts_gdf = gpd.read_file(f"{DATASET_DIR}/tl_2023_48_tract/tl_2023_48_tract.shp")
+    census_tracts_columns = ['GEOID', 'geometry']
+    census_tracts_gdf = census_tracts_gdf[census_tracts_columns]
+    
+    # load NRI data
+    nri_df = pd.read_csv(f"{DATASET_DIR}/NRI/NRI_Table_CensusTracts_Texas.csv")
+    nri_df = nri_df[nri_columns]
 
     # Initialize a list to store the results
     results = []
 
-    for _, tract in census_tracts.iterrows():
+    for _, tract in census_tracts_gdf.iterrows():
         # Extract the geometry in GeoJSON format
         geom = [tract['geometry'].__geo_interface__]
 
@@ -56,23 +61,27 @@ if __name__ == "__main__":
     gdf = gpd.GeoDataFrame(
         results,
         geometry='geometry',
-        crs=census_tracts.crs  # Use the same CRS as the census tracts
+        crs=census_tracts_gdf.crs  # Use the same CRS as the census tracts
     )
+    gdf['GeoId'] = gdf['GeoId'].astype("int64")
+    # merge NRI data with census tract data
+    merged_gdf = gdf.merge(
+        nri_df, left_on='GeoId', right_on='TRACTFIPS', how='left')
+    
+    # clean null rows
+    merged_gdf = merged_gdf.dropna(subset=nri_columns)
 
     for feature_dict in feature_dict_list:
-        add_column_by_feature(gdf, feature_dict)
+        add_column_by_feature(merged_gdf, feature_dict)
         # add_column_by_feature(gdf, feature_dict["reprojected_file_name"], feature_dict["feature_name"])
 
     # change columns from string to float
-    gdf['Housing-Unit-Risk'] = gdf['Housing-Unit-Risk'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
-    gdf['Housing-Unit-Exposure'] = gdf['Housing-Unit-Exposure'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
-    gdf['Housing-Unit-Impact'] = gdf['Housing-Unit-Impact'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
 
-    gdf['Min-Temperature'] = gdf['Min-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
-    gdf['Max-Temperature'] = gdf['Max-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
-    gdf['Mean-Temperature'] = gdf['Mean-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
+    merged_gdf['Min-Temperature'] = merged_gdf['Min-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
+    merged_gdf['Max-Temperature'] = merged_gdf['Max-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
+    merged_gdf['Mean-Temperature'] = merged_gdf['Mean-Temperature'].apply(lambda x: np.nan if np.ma.is_masked(x) else x).astype("float64")
 
-    gdf = gdf.convert_dtypes()
-    print(gdf.dtypes)
+    merged_gdf = merged_gdf.convert_dtypes()
+    print(merged_gdf.dtypes)
 
-    gdf.to_file(f"{DATASET_DIR}/results.geojson", driver='GeoJSON')
+    merged_gdf.to_file(f"{DATASET_DIR}/results.geojson", driver='GeoJSON')
